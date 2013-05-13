588cce8d980b67b4bc6f6885573355593a0cc43b
Gabriel <nyteshade@gmail.com>
2013-05-12 23:27:38 -0700

    Initial commit of base express server using 3.2.4 with scss, less, jquery and q

---
+++ app.js
@@ -0,0 +1,48 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , http = require('http')
+  , path = require('path')
+  , sass = require("node-sass")
+  , app = express();
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'html');
+  app.engine('html', require('hbs').__express);
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(require('less-middleware')({ 
+    src: '../less',
+    dest: '/css',
+    root: path.join(__dirname, 'public'),
+    debug: false
+  }));
+  app.use(sass.middleware({
+    src: '../sass',
+    dest: '/css',
+    root: path.join(__dirname, 'public'),
+    debug: true
+  }));
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/', routes.index);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
+
+exports.app = app;
