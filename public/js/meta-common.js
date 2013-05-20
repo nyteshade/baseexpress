4dfc452492b7450656c819c0008d55e47bb96d55
Gabriel Harrison <nyteshade@gmail.com>
2013-05-20 09:52:51 -0700

    Missing files from last commit

---
+++ public/js/meta-common.js
@@ -0,0 +1,1 @@
+// @require ['q.js', 'jquery-1.9.1.js', 'isA.js', 'bootstrap.min.js']
---
+++ public/js/pages/index.js
@@ -0,0 +1,3 @@
+// @require ['q.js', 'jquery-1.9.1.js', 'isA.js', 'bootstrap.min.js']
+
+window.pagesIndex = true;
