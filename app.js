fd35a42c3d086eb5917153968a88f63e5306b2d4
Gabriel Harrison <nyteshade@gmail.com>
2013-05-20 09:51:31 -0700

    Working on combiner. Old version somewhat broken version in middleware as well

--- app.js
+++ app.js
@@ -3,36 +3,56 @@
  * Module dependencies.
  */
 
+global.dir = function(o,print) {
+  var a=require('util').inspect(o,{colors:true});
+  if(print) 
+    console.log(a); 
+  else 
+    return a;
+};
+
 var express = require('express')
   , routes = require('./routes')
   , http = require('http')
-  , path = require('path')
   , sass = require("node-sass")
   , isA = require('isa-lib')(global)
-  , app = express();
+  , app = express()
+  , fs = require('fs')
+  , path = require('path')
+  , ROOT = path.dirname(__filename)
+  , combiner = require('./middleware/combiner.js')({
+      root: path.join(ROOT, 'public'),
+      jsRoot: path.join(ROOT, 'public', 'js'),
+      cssRoot: path.join(ROOT, 'public', 'css'),
+      env: app.get('env'),
+      express: app
+    })
+  , lessMiddleware = require('less-middleware')({ 
+      src: '../less',
+      dest: '/css',
+      root: path.join(__dirname, 'public'),
+      debug: false
+    })
+  , sassMiddleware = sass.middleware({
+      src: '../sass',
+      dest: '/css',
+      root: path.join(__dirname, 'public'),
+      debug: false
+    });
 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'html');
+  app.set('root', path.resolve(__dirname));
   app.engine('html', require('hbs').__express);
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
+  app.use(lessMiddleware);
+  app.use(sassMiddleware);
   app.use(app.router);
-  app.use(require('less-middleware')({ 
-    src: '../less',
-    dest: '/css',
-    root: path.join(__dirname, 'public'),
-    debug: false
-  }));
-  app.use(sass.middleware({
-    src: '../sass',
-    dest: '/css',
-    root: path.join(__dirname, 'public'),
-    debug: false
-  }));
   app.use(express.static(path.join(__dirname, 'public')));
 });
 
@@ -40,10 +60,13 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
-app.get('/', routes.index);
+app.get('/', combiner.PageNameCombiner, routes.index);
+
+combiner.handleScriptAndStyle(app);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
+  console.log("\x1b[33m" + app.get('env') + "\x1b[0m mode");
 });
 
 // Require the app.js in REPL to bootstrap the app in interactive mode
--- routes/index.js
+++ routes/index.js
@@ -5,3 +5,24 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
+
+exports.index_packages = function(req, res, next) {
+  var combiner = require('js-combiner')({
+    files: [
+      '/q.js',
+      '/jquery-1.9.1.js',
+      '/bootstrap.min.js',
+      '/isA.js'
+    ],
+    minify: false,
+    log: true,
+    packedSuffix: 'packed',
+    folder: '/public/js',
+    cwd: req.app.get('root'),
+    outputName: 'combined'
+  });
+
+  combiner.promise.done(function() {
+    next();
+  });
+}
